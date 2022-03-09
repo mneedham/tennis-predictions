0d043ac90d593f4004016235fd235df7348cab51
Mark Needham <m.h.needham@gmail.com>
2022-03-09 21:32:40 +0000

    indian wells

--- predictions.py
+++ predictions.py
@@ -4,13 +4,13 @@ import json
 
 with open("template.html", "r") as template_file, \
      open("output/predictions.html", "w") as predictions_output_file, \
-     open("data/ausopen2022.json", "r") as predictions_json_file: 
+     open("data/indianwells2022.json", "r") as predictions_json_file: 
     predictions = json.load(predictions_json_file)
     print(predictions)
 
     template = Template(template_file.read())
     output = template.render(
-        tournament='Australian Open', 
-        predictions=predictions
+        tournament=predictions["tournament"], 
+        predictions=predictions["events"]
     )
     predictions_output_file.write(output)
