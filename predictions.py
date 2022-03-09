98e86072577219b94e541f10c935324ed6ff7230
Mark Needham <m.h.needham@gmail.com>
2022-03-09 22:10:42 +0000

    updates

--- predictions.py
+++ predictions.py
@@ -2,17 +2,18 @@ from jinja2 import Template
 import csv
 import json
 
-tournament_name = "indianwells2022"
+tournaments = ["ausopen2022", "indianwells2022"]
 
-with open("template.html", "r") as template_file, \
-     open(f"output/{tournament_name}.html", "w") as predictions_output_file, \
-     open(f"data/{tournament_name}.json", "r") as predictions_json_file: 
-    predictions = json.load(predictions_json_file)
-    print(predictions)
+for tournament_name in tournaments:
+    with open("template.html", "r") as template_file, \
+         open(f"output/{tournament_name}.html", "w") as predictions_output_file, \
+         open(f"data/{tournament_name}.json", "r") as predictions_json_file: 
+        predictions = json.load(predictions_json_file)
+        print(predictions)
 
-    template = Template(template_file.read())
-    output = template.render(
-        tournament=predictions["tournament"], 
-        predictions=predictions["events"]
-    )
-    predictions_output_file.write(output)
+        template = Template(template_file.read())
+        output = template.render(
+            tournament=predictions["tournament"], 
+            predictions=predictions["events"]
+        )
+        predictions_output_file.write(output)
