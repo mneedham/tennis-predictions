37dde55eb67d67e357d5b8e7cebec3c81c45ce5d
Mark Needham <m.h.needham@gmail.com>
2022-05-08 20:51:45 +0100

    Update predictions.py

--- predictions.py
+++ predictions.py
@@ -7,7 +7,7 @@ now = datetime.datetime.utcnow()
 
 tournaments = ["ausopen2022", "indianwells2022", "miami2022", "charleston2022", "montecarlo2022", 
                "barcelona2022", "serbiaopen2022", "stuttgart2022", "munich2022", "estoril2022",
-               "madridopen2022"
+               "madridopen2022", "rome2022"
               ]
 
 for tournament_name in tournaments:
