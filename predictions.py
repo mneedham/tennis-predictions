2f4b9edfae7e0fb740a2330b7e4263fd81e8257b
Mark Needham <m.h.needham@gmail.com>
2022-05-28 10:08:07 +0100

    automate the generation a bit more

--- predictions.py
+++ predictions.py
@@ -2,6 +2,7 @@ from jinja2 import Template
 import csv
 import json
 import datetime
+import glob
 
 now = datetime.datetime.utcnow()
 
@@ -10,10 +11,13 @@ tournaments = ["ausopen2022", "indianwells2022", "miami2022", "charleston2022", 
                "madridopen2022", "rome2022", "frenchopen2022"
               ]
 
-for tournament_name in tournaments:
+tournaments = glob.glob("data/*.json")
+
+for tournament_file_path in tournaments:
+    tournament_name = tournament_file_path.split("/")[-1].replace(".json", "")
     with open("template.html", "r") as template_file, \
          open(f"output/{tournament_name}.html", "w") as predictions_output_file, \
-         open(f"data/{tournament_name}.json", "r") as predictions_json_file: 
+         open(f"{tournament_file_path}", "r") as predictions_json_file: 
         predictions = json.load(predictions_json_file)
         print(predictions)
 
