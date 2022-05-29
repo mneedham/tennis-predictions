af983deb19569af07d8c2201304d4edd0b7702c6
Mark Needham <m.h.needham@gmail.com>
2022-05-29 19:25:21 +0100

    static files

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ app = Flask(__name__, static_folder='../build', static_url_path='/')
 def home():
     return {
         "currentTime": datetime.datetime.now(),
-        "currentTime2": datetime.datetime.now()
+        "currentTime_": datetime.datetime.now()
     }
 
 @app.route('/')
