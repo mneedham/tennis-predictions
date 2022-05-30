0cecaf9a8a6308dd237a5682592b88b13a8c00e0
Mark Needham <m.h.needham@gmail.com>
2022-05-30 07:53:07 +0100

    try another fn2

--- api/index.py
+++ api/index.py
@@ -1,7 +1,26 @@
+# from flask import Flask
+# import datetime
+
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def home():
+#     return {
+#         "currentTime": datetime.datetime.now(),
+#         "currentTime_": datetime.datetime.now()
+#     }
+
+
 from flask import Flask, Response
+import datetime
+
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+        return {
+        "currentTime": datetime.datetime.now(),
+        "currentTime_": datetime.datetime.now()
+    }
