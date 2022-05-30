3e86059182b3b5a3622bfc0650c9d646da3a09a1
Mark Needham <m.h.needham@gmail.com>
2022-05-30 08:24:34 +0100

    try one file again

--- api/index.py
+++ api/index.py
@@ -1,7 +1,8 @@
-from flask import Flask, Response
+from flask import Flask, jsonify
 import datetime
 
 app = Flask(__name__)
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -11,3 +12,14 @@ def catch_all(path):
         "currentTime_": datetime.datetime.now(),
         "path": path
     }
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def tournaments(path):
+    return jsonify([{
+        "name": "Australian Open 2022"
+    },
+        {
+        "name": "French Open 2022"
+    }])
