12be67d8dd4a4134c9b781feb83977ca6999b72c
Mark Needham <m.h.needham@gmail.com>
2022-06-07 21:38:23 +0100

    dummy change

--- frontend/src/components/hero-banner.js
+++ frontend/src/components/hero-banner.js
@@ -17,7 +17,7 @@ export const HeroBanner = () => {
       <h1 className="hero-banner__headline">Hello, React World!</h1>
       <p className="hero-banner__description">
         This is a sample application that demonstrates the authentication flow
-        for a React app using <strong>Auth0</strong>.
+        for a React app using <strong>Auth0</strong>. It is awesome.
       </p>
 
       <button onClick={openCodeSample} className="button button--secondary">
