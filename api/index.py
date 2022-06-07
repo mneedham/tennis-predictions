3fc22c964ebf3d8dccfbcf41f9611098d165fa83
Mark Needham <m.h.needham@gmail.com>
2022-06-07 06:40:41 +0100

    tournaments as its own path

--- api/index.py
+++ api/index.py
@@ -7,19 +7,18 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return {
-        "currentTime": datetime.datetime.now(),
-        "currentTime_": datetime.datetime.now(),
-        "path": path
-    }
+  return {
+    "currentTime": datetime.datetime.now(),
+    "currentTime_": datetime.datetime.now(),
+    "path": path
+  }
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/tournaments', defaults={'path': ''})
 def tournaments(path):
-    return jsonify([{
-        "name": "Australian Open 2022"
-    },
-        {
-        "name": "French Open 2022"
-    }])
+  return jsonify([{
+    "name": "Australian Open 2022"
+  },
+  {
+    "name": "French Open 2022"
+  }])
