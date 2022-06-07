afe18bc481244eb93acf0a09a7340c380e59edab
Mark Needham <m.h.needham@gmail.com>
2022-06-07 07:32:48 +0100

    add headers

--- api/index.py
+++ api/index.py
@@ -4,6 +4,17 @@ import datetime
 app = Flask(__name__)
 
 
+@app.after_request
+def add_headers(response):
+    response.headers['X-XSS-Protection'] = '0'
+    response.headers['Cache-Control'] = 'no-store, max-age=0, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    response.headers['Content-Type'] = 'application/json; charset=utf-8'
+    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+    return response
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
