32340429d765c9d8a85517039df3381e31b76e85
Mark Needham <m.h.needham@gmail.com>
2022-06-07 07:23:13 +0100

    add name

--- api/index.py
+++ api/index.py
@@ -1,38 +1,24 @@
-# from flask import Flask, jsonify
-# import datetime
+from flask import Flask, jsonify
+import datetime
 
-# app = Flask(__name__)
-
-
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def catch_all(path):
-#   return {
-#     "currentTime": datetime.datetime.now(),
-#     "currentTime_": datetime.datetime.now(),
-#     "path": path
-#   }
+app = Flask(__name__)
 
 
-# @app.route('/tournaments', defaults={'path': ''})
-# def tournaments(path):
-#   return jsonify([{
-#     "name": "Australian Open 2022"
-#   },
-#   {
-#     "name": "French Open 2022"
-#   }])
-
-from sanic import Sanic
-from sanic.response import json
-app = Sanic(name="tennis-predictions")
-
-
-@app.route('/')
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-async def index(request, path=""):
-    return json({'hello': path})
+def catch_all(path):
+  return {
+    "currentTime": datetime.datetime.now(),
+    "currentTime_": datetime.datetime.now(),
+    "path": path
+  }
 
-@app.route('/other_route')
-async def other_route(request, path=""):
-    return json({'whatever': path})
+
+@app.route('/tournaments', defaults={'path': ''})
+def tournaments(path):
+  return jsonify([{
+    "name": "Australian Open 2022"
+  },
+  {
+    "name": "French Open 2022"
+  }])
