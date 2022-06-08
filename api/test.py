9688f66bd769bc59132e3a40d82daf6b6406c5f6
Mark Needham <m.h.needham@gmail.com>
2022-06-08 13:09:37 +0100

    test endpoint

--- api/test.py
+++ api/test.py
@@ -6,6 +6,7 @@ public_key = b"-----BEGIN PUBLIC KEY-----\nMHYwEAYHKoZIzj0CAQYFK4EEAC..."
 app = Flask(__name__)
 
 @app.route("/", defaults={'path': ''})
+@app.route('/<path:path>')
 def catch_all(path):
   value = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzb21lIjoicGF5bG9hZCJ9.4twFt5NiznN84AWoo1d7KO1T_yoc0Z6XOpOVswacPZg"
   decoded = jwt.decode(value, public_key, algorithms=["RS256"])
