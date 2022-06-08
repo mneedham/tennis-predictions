baf64114546996d3da5b34e1eb9971e425177d6b
Mark Needham <m.h.needham@gmail.com>
2022-06-08 13:24:25 +0100

    try this instead

--- api/test.py
+++ api/test.py
@@ -1,13 +1,27 @@
 from flask import Flask
 import jwt
+from functools import wraps
 
-public_key = b"-----BEGIN PUBLIC KEY-----\nMHYwEAYHKoZIzj0CAQYFK4EEAC..."
+def authorization_guard(function):
+    @wraps(function)
+    def decorator(*args, **kwargs):
+        public_key = b"-----BEGIN PUBLIC KEY-----\nMHYwEAYHKoZIzj0CAQYFK4EEAC..."
+        value = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzb21lIjoicGF5bG9hZCJ9.4twFt5NiznN84AWoo1d7KO1T_yoc0Z6XOpOVswacPZg"
+        
+        try:
+          jwt.decode(value, public_key, algorithms=["RS256"])
+        except Exception as error:
+          print(error)
+
+        return function(*args, **kwargs)
+
+    return decorator
 
 app = Flask(__name__)
 
 @app.route("/", defaults={'path': ''})
 @app.route('/<path:path>')
+@authorization_guard
 def catch_all(path):
-  value = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzb21lIjoicGF5bG9hZCJ9.4twFt5NiznN84AWoo1d7KO1T_yoc0Z6XOpOVswacPZg"
-  decoded = jwt.decode(value, public_key, algorithms=["RS256"])
-  return decoded
+  
+  return {"decoded": True}
