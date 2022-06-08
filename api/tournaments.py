6041b86e81652de18fceb2bd4b063797b480cb19
Mark Needham <m.h.needham@gmail.com>
2022-06-08 22:57:52 +0100

    two routes in one file?

--- api/tournaments.py
+++ api/tournaments.py
@@ -4,10 +4,14 @@ from utils.scaffolding import create_app
 bp_name = 'api-tournaments'
 bp = Blueprint(bp_name, __name__)
 
+@bp.route('/<tournament_id>')
+def tournaments(tournament_id):
+  return {
+    "name": tournament_id
+  }
 
-@bp.route('/', defaults={'path': ''})
-@bp.route('/<path:path>')
-def tournaments(path):
+@bp.route('/')
+def all_tournaments():
   return jsonify([{
       "name": "Australian Open 2022",
       "shortName": "australian-open-2022"
