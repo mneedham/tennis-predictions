ee8780de9cbcd603a7348f672b099bad537dee07
Mark Needham <m.h.needham@gmail.com>
2022-06-08 13:00:15 +0100

    test endpoint

---
+++ api/test.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import jwt
+
+public_key = b"-----BEGIN PUBLIC KEY-----\nMHYwEAYHKoZIzj0CAQYFK4EEAC..."
+
+app = Flask(__name__)
+
+@app.route("<value>")
+def catch_all(value):
+  decoded = jwt.decode(value, public_key, algorithms=["RS256"])
+  return decoded
