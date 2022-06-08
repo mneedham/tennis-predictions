dd93ca8cce4a71bfda7e4f14129d8ebbf557c30c
Mark Needham <m.h.needham@gmail.com>
2022-06-08 13:11:44 +0100

    tournamentsg= endpoint

--- api/tournaments.py
+++ api/tournaments.py
@@ -6,6 +6,7 @@ bp = Blueprint(bp_name, __name__)
 
 
 @bp.route('/', defaults={'path': ''})
+@bp.route('/<path:path>')
 def tournaments(path):
   return jsonify([{
       "name": "Australian Open 2022",
