106d356fad05cee15ba0b941ad5d3fd9c7089df2
Mark Needham <m.h.needham@gmail.com>
2022-06-21 22:21:19 +0100

    testing out catch all

--- api/test.py
+++ api/test.py
@@ -2,21 +2,6 @@ from flask import Flask, request
 import jwt
 from functools import wraps
 
-def authorization_guard(function):
-    @wraps(function)
-    def decorator(*args, **kwargs):
-        public_key = b"-----BEGIN PUBLIC KEY-----\nMHYwEAYHKoZIzj0CAQYFK4EEAC..."
-        value = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzb21lIjoicGF5bG9hZCJ9.4twFt5NiznN84AWoo1d7KO1T_yoc0Z6XOpOVswacPZg"
-        
-        try:
-          jwt.decode(value, public_key, algorithms=["RS256"])
-        except Exception as error:
-          print(error)
-
-        return function(*args, **kwargs)
-
-    return decorator
-
 app = Flask(__name__)
 
 inner_app = Flask("Inner Flask")
@@ -37,8 +22,7 @@ def fallback(first=None, rest=None):
     return 'This one catches everything else'
 
 @app.route("/", defaults={'path': ''})
-@app.route('/<path:path>', methods=["GET", "POST"])
-@authorization_guard
+@app.route('/<path:path>')
 def catch_all(path):
   inner_response = inner_app.test_client().open(request)
   return inner_response.get_data()
